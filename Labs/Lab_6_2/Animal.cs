8f57c6430b75159ab9027cba0a7b5340f6a63cd2
Misha <sheb70017@gmail.com>
2019-12-25 13:53:33 +0300

    It is All over!

---
+++ Labs/Lab_6_2/Animal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_6_2
+{
+    class Animal
+    {
+        public Animal()
+        {
+            Name = "Шарик";
+            Size = 4;
+        }
+        public Animal(int size)
+        {
+            Name = "Шарик";
+            Size = size;
+        }
+        public Animal(string name)
+        {
+            Name = name;
+            Size = 4;
+        }
+
+        [AttributeClass("Это просто размер")]
+        public int Size { get; private set; } // в условных единицах
+        [AttributeClass(Description = "А это имя")]
+        public string Name { get; set; }
+
+        public void Move()
+        {
+            Console.WriteLine("Keep moving");
+        }
+        public void Alive()
+        {
+            Console.WriteLine("It is alive!");
+        }
+
+    }
+}
